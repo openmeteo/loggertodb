bab7938b1a546faac7d2ac187cbe255701c25406
Antonis Christofides <antonis@antonischristofides.com>
2019-06-07 16:37:40 +0300

    Version 0.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ author = u"Antonis Christofides"
 # the built documents.
 #
 # The short X.Y version.
-version = "0.1.2"
+version = "0.1.3"
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- loggertodb/__init__.py
+++ loggertodb/__init__.py
@@ -1,4 +1,4 @@
 from .exceptions import *  # NOQA
 from .meteologgerstorage import *  # NOQA
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
